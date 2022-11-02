948677d13cbb71c807d099df8512d2e9c4135aa1
Rakshat Kashyap <kashyaprakshat@gmail.com>
2022-11-02 11:27:03 +0530

    added routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import components from "./components";
 
-function App() {
-  const [count, setCount] = useState(0)
+const { Landing, Dashboard, AddTopic } = components;
 
+const App = () => {
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/add-topic" element={<AddTopic />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
